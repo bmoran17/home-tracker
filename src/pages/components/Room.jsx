751dd37aee01a0057561280ecc0fb7bdecdff91a
bmoran17 <brendamoran1999@gmail.com>
2025-07-17 21:51:38 -0700

    Added form to add items

--- src/pages/components/Room.jsx
+++ src/pages/components/Room.jsx
@@ -1,6 +1,17 @@
+import "./Room.css";
+
 export default function Room(props) {
     const data = props.data;
     const room = props.room;
+    
+    function addItem() {
+        const form = document.getElementById("addItemForm");
+        if (form.style.display === "none") {
+            form.style.display = "block";
+        } else {
+            form.style.display = "none";
+        }
+    }
     
     return(
         <>
@@ -16,19 +27,41 @@ export default function Room(props) {
             </tr>
             <tbody>
                 {Object.entries(data).map(([key, item]) => (
-                    <tr>
+                    <tr key={key}>
                         <td><img src={item.photo} alt={item.name} width={50} height={50} /></td>
                         <td>{item.name}</td>
                         <td><a href={item.link}>click</a></td>
                         <td>{item.quantity}</td>
                         <td>{item.lastPurchase}</td>
                         <td>{item.need ? "Yes" : "No"}</td>
-                        <td><button>Edit</button></td>
+                        <td><button className="editBtn">Edit</button></td>
                     </tr>
             ))}
                 </tbody>
         </table>
-        <button>Add Item</button>
+        <form id="addItemForm" style={{display: "none"}}>
+            <label>
+                Item Name:
+            <input type="text" name="itemName" required />
+            </label>
+            <label>
+                Link:
+                <input type="text" name="link" />
+            </label>
+            <label>
+                Quantity:
+                <input type="number" name="quantity" />
+            </label>
+            <label>
+                Last Purchase:
+                <input type="date" name="lastPurchase" />
+            </label>
+            <label>
+                Need:
+                <input type="checkbox" name="need" />
+            </label>
+        </form>
+        <button onClick={addItem} type="submit">Add Item</button>
         </>  
     );
 } 
